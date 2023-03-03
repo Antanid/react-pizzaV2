e9fbb59b37ea9faefe35e2b581d30451e9951360
Antanid <dbalakleenko1@gmail.com>
2023-03-03 16:53:30 +0200

    .

--- src/redux/pizza/asyncActions.ts
+++ src/redux/pizza/asyncActions.ts
@@ -6,7 +6,7 @@ import { FetchType, PizzaType } from "./types";
 export const fetchPizzas = createAsyncThunk("pizzaSlice/fetchPizzasStatus ", async (params: FetchType) => {
     const { IndexCategory, sortNameRep, sortType, search, pagePagination } = params;
     const pizzaArr = await axios.get<PizzaType[]>(
-      `https:63dc22b6b8e69785e49282a5.mockapi.io/pizza?page=${pagePagination}
+      `https://63dc22b6b8e69785e49282a5.mockapi.io/pizza?page=${pagePagination}
                    &limit=4&${IndexCategory}&sortBy=${sortNameRep}&order=${sortType}&${search}`
     );
     return pizzaArr.data as PizzaType[];
